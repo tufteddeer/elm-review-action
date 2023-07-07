092a4bd067ac14eba3d96d845e400c78e55929f8
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-07-07 11:21:03 +0200

    debug stuff4

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,7 @@ const runElmReview = async (): Promise<ReviewErrors | CliError> => {
     silent: true
   }
 
-  core.setFailed("args: " + elmReviewArgs())
+  throw("args: " + elmReviewArgs())
 
   await exec.exec(inputElmReview, elmReviewArgs(), options)
 
